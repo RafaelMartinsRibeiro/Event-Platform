a3fb07c33c9c2f100605264eccec6f7c6a3a72ed
RafaelMartinsRibeiro <rafaelmartins1964@hotmail.com>
2022-09-28 22:29:16 -0300

    Added Lessons query in Sidebar component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,31 @@
+import { gql, useQuery } from "@apollo/client";
 import { Lesson } from "./Lesson";
 
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
+      id
+      lessonType
+      slug
+      title
+      availableAt
+    }
+  }
+`;
+
+interface GetLessonsQueryResponse {
+  lessons: Array<{
+    availableAt: string;
+    id: string;
+    lessonType: "live" | "class";
+    slug: string;
+    title: string;
+  }>;
+}
+
 export const Sidebar = () => {
+  const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
+
   return (
     <aside className="w-[348px] p-6 bg-gray-700 border-l border-gray-600">
       <span className="text-center font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">
@@ -8,12 +33,17 @@ export const Sidebar = () => {
       </span>
 
       <div className="flex flex-col gap-8">
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
-        <Lesson />
+        {data?.lessons.map((lesson) => {
+          return (
+            <Lesson
+              key={lesson.id}
+              title={lesson.title}
+              slug={lesson.slug}
+              availableAt={new Date(lesson.availableAt)}
+              type={lesson.lessonType}
+            />
+          );
+        })}
       </div>
     </aside>
   );
